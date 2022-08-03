fb5a5f462cce0c4f72483ef422d7919d4f33fca1
Usuario Campo Mourao <uept11-user@aluno.pr.senac.br>
2022-08-03 10:09:14 -0300

    Arrumei os codigos

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-
-
-function Exibironome(){
-    let nome=document.getElementById("campo-nome").value
-    alert(nome)
+function Exibironome() {
+    let nome = document.getElementById("campo-nome").value;
+    alert(nome);
 }
-function Exibironome(){
-    let nome = document.getElementById("c0-campo-nome").value
-    document.getElementById("c0-resultado").innerHTML = "Oi tudo bem?, " + nome+ "!"
- 
+function Exibironome() {
+    let nome = document.getElementById("c0-campo-nome").value;
+    document.getElementById("c0-resultado").innerHTML =
+        "Oi tudo bem?, " + nome + "!";
+}
 
- }
-
-
- function exibeNumero(){
-    let numero = document.getElementById("c1-campo-numero").value
-    if(numero %2 ==0){
-        document.getElementById("c1-resultado").innerHTML = "Numero par"
-        }else { document.getElementById("c1-resultado").innerHTML = "Numero impar "
+function exibeNumero() {
+    let numero = document.getElementById("c1-campo-numero").value;
+    if (numero % 2 == 0) {
+        document.getElementById("c1-resultado").innerHTML = "Numero par";
+    } else {
+        document.getElementById("c1-resultado").innerHTML = "Numero impar ";
     }
-    
- }
+}
 
+function aleatorio() {
+    let min = parseFloat(document.getElementById("c-2campo-numerico").value);
+    let max = parseFloat(document.getElementById("campo-numerico01").value);
+    let d = parseFloat(document.getElementById("d").value);
+    console.log(typeof min, min, max, d);
 
-
-
-     function aleatorio(){
-        let min = parseFloat(document.getElementById ("c-2campo-numerico").value)
-        let max = parseFloat (document.getElementById ("campo-numerico01").value)
-        let d = parseFloat (document.getElementById ("d").value)
-    console.log(typeof (min),min,max,d)
-    
-    let valAleatorio = []
-    for (let i = 0; i<d; i++){
-    valAleatorio.push (Math.floor(Math.random() * (max - min + 1)) + min)
-    }
-    document.getElementById("c-2resultado").innerHTML = valAleatorio
+    let valAleatorio = [];
+    for (let i = 0; i < d; i++) {
+        valAleatorio.push(Math.floor(Math.random() * (max - min + 1)) + min);
     }
 
-    function exibesuaIdade(){
-        let numero = document.getElementById("c3-campo-numero").value
-        if(numero >18){
-            
-            document.getElementById("c3-resultado").innerHTML = "Você e maior de idade"
-            }else { document.getElementById("c3-resultado").innerHTML = "voce e menor de idade "
-        console.log("Menor de idade")
+    document.getElementById("c-2resultado").innerHTML = valAleatorio;
+}
+
+function exibesuaIdade() {
+    let numero = document.getElementById("c3-campo-numero").value;
+    if (numero > 18) {
+        document.getElementById("c3-resultado").innerHTML =
+            "Você e maior de idade";
+    } else {
+        document.getElementById("c3-resultado").innerHTML =
+            "voce e menor de idade ";
+        console.log("Menor de idade");
+    }
+}
+function Crescente(a, b) {
+    return a - b;
+}
+function GeraJogoMega() {
+    let nJogos = parseFloat(document.getElementById("c4-campo-numerico").value);
+    let JogosMega = [];
+    let Numerossorteados;
+    for (let i = 0; i < nJogos; i++) {
+        Numerossorteados = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
+        if (JogosMega.includes(Numerossorteados)) {
+            Numerossorteados = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
         }
-        
-     }
-     function GeraJogoMega(){
-        let nJogos = parseFloat(document.getElementById("c4-campo-numerico").value)
-      let JogosMega=[]
-        for(let i=0;i<nJogos;i++){
-            JogosMega.push("jogo" + i)
-
-        }
-document.getElementById("c4-resultado").innerHTML=JogosMega
-     }
-     function CriaJogovalido(){
-
-     }
+        JogosMega.push(Numerossorteados);
+    }
+    JogosMega.sort(Crescente);
+    document.getElementById("c4-resultado").innerHTML = JogosMega;
+}
